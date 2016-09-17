9793f0326b2853e3060757b07e097bc0ca44ea99
the7winds <vga.grc@gmail.com>
2016-09-17 15:02:22 +0300

    removed useless methods

--- src/main/java/shell/syntax/Token.java
+++ src/main/java/shell/syntax/Token.java
@@ -1,7 +1,5 @@
 package shell.syntax;
 
-import shell.Environment;
-
 /**
  * Created by the7winds on 10.09.16.
  */
@@ -29,33 +27,5 @@ public class Token {
 
     public Type getType() {
         return type;
-    }
-
-    public void expand() {
-        if (type == Type.ARG) {
-            if (token.charAt(0) == '\"') {
-                token = replaceDollar(token.substring(1, token.length() - 1));
-            }
-            if (token.charAt(0) == '\'') {
-                token = token.substring(1, token.length() - 1);
-            }
-        }
-    }
-
-    private String replaceDollar(String token) {
-        for (int start = 0, end = 0; start < token.length();) {
-            if (token.charAt(start) == '$') {
-                end = start + 1;
-                while (end < token.length() && Character.isLetterOrDigit(token.charAt(end))) {
-                    end++;
-                }
-                String variable = token.substring(start + 1, end);
-                token = token.substring(0, start)
-                        + Environment.getInstance().getValue(variable)
-                        + token.substring(end);
-            }
-        }
-
-        return token;
     }
 }
