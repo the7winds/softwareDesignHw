d80d09627c1cb35f3650e309090e9148064e0b8b
the7winds <vga.grc@gmail.com>
2016-12-04 01:17:02 +0300

    added test

---
+++ src/test/java/chat/model/network/MessengerTest.java
@@ -0,0 +1,52 @@
+package chat.model.network;
+
+import chat.model.network.protocol.P2PMessenger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by the7winds on 04.12.16.
+ */
+public class MessengerTest {
+
+    @Test(timeout = 2000)
+    public void SimpleTest() throws IOException {
+
+        final LinkedList<P2PMessenger.Message> received = new LinkedList<>();
+
+        HandlerObserver receiver = new HandlerObserver(null);
+        receiver.addHandler(P2PMessenger.Message.BodyCase.TEXTMESSAGE, received::add);
+
+        HandlerObserver empty = new HandlerObserver(null);
+
+        int port = 8000;
+
+        Messenger server = new Messenger(port, empty);
+        server.start();
+
+        Messenger client = new Messenger("localhost", port, receiver);
+        client.start();
+
+        String testString = "TEST";
+
+        while (!server.isConnected());
+
+        server.sendMessage(P2PMessenger.Message.newBuilder()
+                .setTextMessage(
+                        P2PMessenger.TextMessage.newBuilder()
+                        .setText(testString)
+                        .build()
+                ).build()
+        );
+
+        while (received.isEmpty());
+
+        assertEquals(testString, received.getFirst().getTextMessage().getText());
+    }
+
+}
