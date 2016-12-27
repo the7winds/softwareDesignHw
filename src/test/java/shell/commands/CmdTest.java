09062607dbe4e0e34e09054265b02058adc61493
LizaTretyakova <elizabet.tretyakova@gmail.com>
2016-12-27 16:56:58 +0300

    Added new test

--- src/test/java/shell/commands/CmdTest.java
+++ src/test/java/shell/commands/CmdTest.java
@@ -1,7 +1,14 @@
 package shell.commands;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import shell.Shell;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.InputStreamReader;
 
 import static org.junit.Assert.*;
 
@@ -9,6 +16,9 @@ import static org.junit.Assert.*;
  * Created by liza on 27.12.16.
  */
 public class CmdTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
     public void testCdLs() throws Exception {
         Shell.runOneCmd("cd .");
@@ -16,4 +26,20 @@ public class CmdTest {
         Shell.runOneCmd("cd ..");
     }
 
+    @Test
+    public void testCdLs1() throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new java.io.PrintStream(out));
+
+        Shell.runOneCmd("cd " + folder.getRoot().getAbsolutePath());
+        Shell.runOneCmd("ls");
+        File file1 = folder.newFolder("meowFile1");
+        Shell.runOneCmd("ls");
+        Shell.runOneCmd("cd ./meowFile1");
+        Shell.runOneCmd("cd fkjhkjhk");
+
+        assertEquals(file1.getAbsolutePath() + "\n" +
+                "./meowFile1 is not a directory\n" +
+                "fkjhkjhk is not a directory\n", out.toString());
+    }
 }
